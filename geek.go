f212cf24233ff58a4a63c8102b37a47af4bdd220
hardPass <hardPass@126.com>
2016-03-28 14:36:39 +0800

    ss

---
+++ geek.go
@@ -0,0 +1,40 @@
+package geek
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+)
+
+const (
+	FormatJsonBorder = "------------\n%s\n------------\n"
+)
+
+var (
+	_dddebuggg = true
+)
+
+func SetDebug(b bool) {
+	_dddebuggg = b
+}
+
+func IsDebug() bool {
+	return _dddebuggg
+}
+
+func DebugF(format string, v ...interface{}) {
+	if IsDebug() {
+		log.Printf(format, v...)
+	}
+}
+
+func DebugJSON(body []byte) {
+	if IsDebug() {
+
+		buf := bytes.NewBuffer(make([]byte, 0, len(body)+1024))
+		if err := json.Indent(buf, body, "", "    "); err == nil {
+			body = buf.Bytes()
+		}
+		log.Printf(FormatJsonBorder, body)
+	}
+}
